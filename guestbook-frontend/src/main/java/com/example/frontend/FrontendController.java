fee99faf49260fa443ecdfd3d3267bfe32f9432e
Sergio Puyó <spuyo@github.com>
2021-02-02 11:12:19 +0100

    Working with runtime configuration

--- guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
+++ guestbook-frontend/src/main/java/com/example/frontend/FrontendController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 import java.util.*;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @Controller
 @SessionAttributes("name")
 public class FrontendController {
